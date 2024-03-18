6282430753d3fc60c67595c34dfb1eca343abd48
Manoj_Pant <manojpant097@gmail.com>
2024-03-18 17:08:43 +0530

    updated Banner code

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,6 +4,7 @@ const Admin = require("../models/admin");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const path = require("path");
 
 const securePassword = async (password) => {
   try {
@@ -32,77 +33,110 @@ const admin_register = async (req, res) => {
 
     const admin_data = await admin.save();
 
-    return res
-      .status(201)
-      .json({
-        success: true,
-        message: "Admin registered successfully,",
-        data: admin_data,
-      });
+    return res.status(201).json({
+      success: true,
+      message: "Admin registered successfully,",
+      data: admin_data,
+    });
   } catch (error) {
     if (error.name === "MongoError" && error.code === 11000) {
-      return res
-        .status(400)
-        .json({
-          success: false,
-          msg: "Duplicate key error. Email already exists.",
-        });
+      return res.status(400).json({
+        success: false,
+        msg: "Duplicate key error. Email already exists.",
+      });
     }
     console.error("Error in registering user", error);
     res.status(500).json({ success: false, message: "Internal Server Error" });
   }
 };
 
+const login_admin = async (req, res) => {
+  try {
+    const { name, password } = req.body;
 
-const login_admin = async(req,res)=>{
-    try {
-        const {name, password} = req.body;
+    const admin = await Admin.findOne({ name: name });
 
-        const admin = await Admin.findOne({ name:name  });
-
-        if (!admin) {
-            return res.status(401).json({ success: false, msg: 'Admin not found' });
-        }
-
-        const isPasswordValid = await bcryptjs.compare(password, admin.password);
-
-        if (!isPasswordValid) {
-            return res.status(401).json({ success: false, msg: 'Incorrect name or password please check' });
-        }
-        const admin_id  = admin._id
-
-        const token = generateAuthToken(admin_id);
-
-        admin.token = token;
-        await admin.save();
-
-        return res.status(200).json({ success: true, msg: 'Login successful', token });
-
-    } catch (error) {
-        return res.status(500).json({ message: 'Internal Server Error' });
+    if (!admin) {
+      return res.status(401).json({ success: false, msg: "Admin not found" });
     }
-}
 
+    const isPasswordValid = await bcryptjs.compare(password, admin.password);
 
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({
+          success: false,
+          msg: "Incorrect name or password please check",
+        });
+    }
+    const admin_id = admin._id;
 
+    const token = generateAuthToken(admin_id);
+
+    admin.token = token;
+    await admin.save();
+
+    return res
+      .status(200)
+      .json({ success: true, msg: "Login successful", token });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 const addBanner = async (req, res) => {
   try {
-    const { image1, image2, image3 } = req.files;
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: "No files were uploaded." });
+    }
+
+    // Construct image paths
+    const bannerImagePath1 = path.join(
+      "images",
+      req.files["image1"][0].filename
+    );
+    const bannerImagePath2 = path.join(
+      "images",
+      req.files["image2"][0].filename
+    );
+    const bannerImagePath3 = path.join(
+      "images",
+      req.files["image3"][0].filename
+    );
+
+    // Construct complete image paths with the new base URL
+    const baseUrl = "http://localhost:3001/";
+
+    const completeBannerImagePath1 = `${baseUrl}${bannerImagePath1.replace(
+      /\\/g,
+      "/"
+    )}`;
+    const completeBannerImagePath2 = `${baseUrl}${bannerImagePath2.replace(
+      /\\/g,
+      "/"
+    )}`;
+    const completeBannerImagePath3 = `${baseUrl}${bannerImagePath3.replace(
+      /\\/g,
+      "/"
+    )}`;
 
     // Create a new banner entry in the database
-    const newBanner = new Banner({
-      image1: image1 ? image1[0].filename : null,
-      image2: image2 ? image2[0].filename : null,
-      image3: image3 ? image3[0].filename : null,
+    const newBanner = await Banner.create({
+      image1: completeBannerImagePath1,
+      image2: completeBannerImagePath2,
+      image3: completeBannerImagePath3,
       banner_id: 1,
     });
-
     await newBanner.save();
-
-    res
-      .status(201)
-      .json({ message: "Banner added successfully.", data: newBanner });
+    res.status(201).json({
+      message: "Banner added successfully.",
+      data: {
+        image1: completeBannerImagePath1,
+        image2: completeBannerImagePath2,
+        image3: completeBannerImagePath3,
+      },
+    });
   } catch (error) {
     console.error("Error adding banner:", error);
     res.status(500).json({ message: "Error adding banner." });
@@ -111,35 +145,73 @@ const addBanner = async (req, res) => {
 
 const updateBanner = async (req, res) => {
   try {
-    const { image1, image2, image3 } = req.files;
 
     // Find the last existing banner with banner_id = 1
-    const lastBanner = await Banner.findOne({ banner_id: 1 }).sort({
+    const existingBanner = await Banner.findOne({ banner_id: 1 }).sort({
       createdAt: -1,
     });
 
-    if (!lastBanner) {
+    if (!existingBanner) {
       return res
         .status(404)
         .json({ message: "No banner found for the specified banner_id." });
     }
 
-    // Update the images for the last banner
-    lastBanner.image1 = image1 ? image1[0].filename : lastBanner.image1;
-    lastBanner.image2 = image2 ? image2[0].filename : lastBanner.image2;
-    lastBanner.image3 = image3 ? image3[0].filename : lastBanner.image3;
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return res.status(400).json({ message: "No files were uploaded." });
+    }
 
-    // Save the changes to the database
-    await lastBanner.save();
+  
+    // Construct image paths
+    const bannerImagePath1 = path.join(
+      "images",
+      req.files["image1"][0].filename
+    );
+    const bannerImagePath2 = path.join(
+      "images",
+      req.files["image2"][0].filename
+    );
+    const bannerImagePath3 = path.join(
+      "images",
+      req.files["image3"][0].filename
+    );
 
-    res
-      .status(200)
-      .json({ message: "Banner updated successfully.", data: lastBanner });
+    // Construct complete image paths with the new base URL
+    const baseUrl = "http://localhost:3001/";
+
+    const completeBannerImagePath1 = `${baseUrl}${bannerImagePath1.replace(
+      /\\/g,
+      "/"
+    )}`;
+    const completeBannerImagePath2 = `${baseUrl}${bannerImagePath2.replace(
+      /\\/g,
+      "/"
+    )}`;
+    const completeBannerImagePath3 = `${baseUrl}${bannerImagePath3.replace(
+      /\\/g,
+      "/"
+    )}`;
+
+    // Update the existing banner entry in the database
+    existingBanner.image1 = completeBannerImagePath1;
+    existingBanner.image2 = completeBannerImagePath2;
+    existingBanner.image3 = completeBannerImagePath3;
+
+    await existingBanner.save();
+
+    res.status(200).json({
+      message: "Banner updated successfully.",
+      data: {
+        image1: completeBannerImagePath1,
+        image2: completeBannerImagePath2,
+        image3: completeBannerImagePath3,
+      },
+    });
   } catch (error) {
     console.error("Error updating banner:", error);
     res.status(500).json({ message: "Error updating banner." });
   }
-};
+}
 
 const getBanner = async (req, res) => {
   try {
